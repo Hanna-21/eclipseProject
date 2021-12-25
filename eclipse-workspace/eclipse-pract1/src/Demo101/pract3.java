ad4410e0df15d945dc4b9a2b5fb73b1fe799bfe3
Hanna-21 <hanna@macintosh.home>
2021-12-25 14:59:23 -0600

    First commit

---
+++ eclipse-workspace/eclipse-pract1/src/Demo101/pract3.java
@@ -0,0 +1,10 @@
+package Demo101;
+
+public class pract3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("How are you all doing?");
+	}
+
+}
